250e0549ec672116358ce01663518b6df7ec3c95
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-10-02 21:13:16 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
 use chip8::Chip8;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    file: PathBuf,
+}
 
 fn main() -> Result<()> {
-    let mut chip8 = Chip8::load("tests/test_opcode.ch8")?;
+    let args = Cli::parse();
+    let mut chip8 = Chip8::load(args.file)?;
     chip8.run()?;
     Ok(())
 }
